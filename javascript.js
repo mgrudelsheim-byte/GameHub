371a9c7a060c8a7f040db16532ca86824c78d1f9
mgrudelsheim-byte <mgrudelsheim@gmail.com>
2026-03-26 19:14:44 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,97 @@
+function dropdownmenu1()
+{
+	
+const ster = document.getElementById("span1");
+
+if (ster.innerHTML === "★") {
+    ster.innerHTML = "☆";
+} else {
+    ster.innerHTML = "★";
+}
+	
+
+}
+function dropdownmenu2()
+{
+	
+const ster1 = document.getElementById("span1");
+const ster2 = document.getElementById("span2");
+
+	
+	if (ster1.innerHTML === "★") {
+    ster1.innerHTML = "☆";
+	ster2.innerHTML = "☆";
+} else {
+    ster1.innerHTML = "★";
+	ster2.innerHTML = "★";
+}
+}
+
+function dropdownmenu3()
+{
+	
+const ster1 = document.getElementById("span1");
+const ster2 = document.getElementById("span2");
+const ster3 = document.getElementById("span3");
+
+
+	if (ster1.innerHTML === "★") {
+    ster1.innerHTML = "☆";
+	ster2.innerHTML = "☆";
+	ster3.innerHTML = "☆";
+} else {
+    ster1.innerHTML = "★";
+	ster2.innerHTML = "★";
+	ster3.innerHTML = "★";	
+}
+}
+function dropdownmenu4()
+{
+	
+
+const ster1 = document.getElementById("span1");
+const ster2 = document.getElementById("span2");
+const ster3 = document.getElementById("span3");
+const ster4 = document.getElementById("span4");
+
+
+
+if (ster1.innerHTML === "★") {
+    ster1.innerHTML = "☆";
+	ster2.innerHTML = "☆";
+	ster3.innerHTML = "☆";
+	ster4.innerHTML = "☆";
+} else {
+    ster1.innerHTML = "★";
+	ster2.innerHTML = "★";
+	ster3.innerHTML = "★";
+	ster4.innerHTML = "★";
+
+}}
+function dropdownmenu5()
+{
+	
+const ster1 = document.getElementById("span1");
+const ster2 = document.getElementById("span2");
+const ster3 = document.getElementById("span3");
+const ster4 = document.getElementById("span4");
+const ster5 = document.getElementById("span5");
+
+
+
+if (ster1.innerHTML === "★") {
+    	ster1.innerHTML = "☆";
+	ster2.innerHTML = "☆";
+	ster3.innerHTML = "☆";
+	ster4.innerHTML = "☆";
+	ster5.innerHTML = "☆";
+} else {
+    ster1.innerHTML = "★";
+	ster2.innerHTML = "★";
+	ster3.innerHTML = "★";
+	ster4.innerHTML = "★";
+	ster5.innerHTML = "★";
+	
+
+}
+}
